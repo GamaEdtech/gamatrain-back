bc10b5a46c3699979fe2ff909e6f497f2853e22e
farsica <fariborz2004@yahoo.com>
2026-07-19 19:09:58 +0330

    school rating

--- src/Infrastructure/Infrastructure/Migrations/20260717233854_RenameRankScoreToRating.cs
+++ src/Infrastructure/Infrastructure/Migrations/20260717233854_RenameRankScoreToRating.cs
@@ -19,6 +19,8 @@ namespace GamaEdtech.Infrastructure.Migrations
                 name: "IX_Schools_RankScore",
                 table: "Schools",
                 newName: "IX_Schools_Rating");
+
+            migrationBuilder.Sql("UPDATE Schools SET Rating=NULL");
         }
 
         /// <inheritdoc />
